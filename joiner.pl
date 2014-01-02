6d6a76e3352f2c5eaba774fc6864b17b0edebed8
Mouq <alexmoquin@gmail.com>
2014-01-02 04:31:11 -0500

    Clean up

--- joiner.pl
+++ joiner.pl
@@ -3,11 +3,14 @@ open my $f_file, '<', 'footer.html';
 local $/; # slurp everything
 my $header = <$h_file>;
 my $footer = <$f_file>;
+chdir('pages');
 for (glob '*.ml') {
     open my $f, '<', $_;
+    chdir('..');
     substr($_, -3) = '.html';
     open my $new, '>', $_;
     print $new $header;
     print $new $_ for <$f>;
     print $new $footer;
+    chdir('pages');
 }
