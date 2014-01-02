d628d906a8a7e571e190aa2d64bc4d9d43746144
Mouq <alexmoquin@gmail.com>
2014-01-02 09:51:15 -0500

    Add fairytale page

---
+++ pages/fairytale.ml
@@ -0,0 +1,3 @@
+<div class='title'>Traits of a Fairytale</div>
+The following is a succint explanation of the <a href='http://www.all-about-fairies.com/origin-of-fairy-tales.html/'>origin of fairytales</a>, accompanied by a
+<a href='http://www.surlalunefairytales.com/introduction/timeline.html'>timeline of fairytales</a>
