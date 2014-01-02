9e1821b7fd1b7fa7eb32fe87f8c3cf1c576e1eb6
Mouq <alexmoquin@gmail.com>
2014-01-02 10:03:55 -0500

    commit

--- pages/misogyny.ml
+++ pages/misogyny.ml
@@ -1,2 +1,3 @@
 <div class='title'>Misogyny<div class='subtitle'>Then and Now</div></div>
+<img src='infographic.png'></img>
 Take our survey and help assess how widespread misogyny is today <a href='http://kwiksurveys.com/app/showpoll.asp?qid=277322&sid=bongy4cgz3jt7zh277322&new=True'>here</a>
