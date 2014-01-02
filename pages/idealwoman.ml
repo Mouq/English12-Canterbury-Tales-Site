47b62efa3e8b3f27c8d009420bab8c45c08a8fb4
Mouq <alexmoquin@gmail.com>
2014-01-02 04:56:32 -0500

    Start work on pages, copy info to idealwoman

---
+++ pages/idealwoman.ml
@@ -0,0 +1,23 @@
+<div class='title'>Ideal Woman</div>
+Medieval woman were supposed to be:
+    <ul>
+    <li>A virgin </li>
+    <li>Beautiful </li>
+    <li>Obedient </li>
+    <li>Silent </li>
+    <li>Moral</li>
+    </ul>
+A women was supposed to be loyal to their husbands, and the wife was supposed to cook and clean.
+<br/>
+<br/>
+The Ideal woman today is: 
+    <ul>
+    <li>A woman to laugh at their jokes </li>
+    <li>2-4 years younger </li>
+    <li>Brunette </li>
+    <li>Long legs </li>
+    <li>Narrow hips </li>
+    <li>A woman who can carry their own weight monetarily</li> 
+    <li>A woman who is educated</li>
+    </ul>
+(according to http://www.menshealth.com/sex-women/the-perfect-woman)
